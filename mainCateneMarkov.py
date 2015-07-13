a3c828c9909b21938f266a584b209f1e0751d60b
maurizio ferrero <drugotosto@gmail.com>
2015-07-13 12:26:24 +0200

    prova di cambiamento!

--- mainCateneMarkov.py
+++ mainCateneMarkov.py
@@ -12,4 +12,4 @@ if __name__=='__main__':
     print "\nCostruzione catena di MArkov del modello dato come progetto finale (modello di un sistema)"
 
     # Definzione #persone per cui si calcolano gli indici di prestazione
-    dim=4
+    dim=2
